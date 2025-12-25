a6f6e12b7a30bcb7b535c3e02711bbf1d675feb6
lakhrouf mohamed <mohamedlakhrouf@gmail.com>
2025-12-25 09:29:36 +0100

    edit index screen

--- frontend/src/services/filmservice.js
+++ frontend/src/services/filmservice.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = "http://192.168.68.122:3000";
+const API_URL = "http://192.168.68.102:3000";
 const STORAGE_KEY = "films_cache";
 
 export const getFilms = async () => {
@@ -10,6 +10,7 @@ export const getFilms = async () => {
     const response = await axios.get(`${API_URL}/films`);
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(response.data));
     return response.data;
+  // eslint-disable-next-line no-unused-vars
   } catch (error) {
     console.log("Offline → loading cached films");
 
