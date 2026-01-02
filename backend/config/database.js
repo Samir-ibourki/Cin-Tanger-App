89f4b0687429c039e337a643d3d32df703b7180b
Samir-ibourki <samiribourki7@gmail.com>
2026-01-02 19:49:20 +0100

    fix railway

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,6 +6,12 @@ const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
       dialect: "postgres",
       logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     })
   : new Sequelize(
       process.env.POSTGRES_DB,
